5dd9a4287ca7a1a5872c09aae163a4910623d7c7
Robin Schramm <wobula@gmail.com>
2017-12-02 15:24:49 -0800

    Creating validation for pipes and quotes.  Quotes are simple.  Pipes require the usage of states.

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -63,7 +63,7 @@ bool	get_close_quote(char *inst, int *index, char quote)
 	return (found_delim);
 }
 
-bool	validate_basic_quotes(char *inst)
+bool	validate_quotes(char *inst)
 {
 	bool	quote_on;
 	int		x;
@@ -72,7 +72,7 @@ bool	validate_basic_quotes(char *inst)
 	quote_on = false;
 	while (inst[++x])
 	{
-		if (inst[x] == 39 || inst[x] == '"')
+		if (inst[x] == 39 || inst[x] == '"' || inst[x] == '`')
 		{
 			quote_on = true;
 			ft_printf("~~Found quote %c at index value: %d\n", inst[x], x);
@@ -80,19 +80,78 @@ bool	validate_basic_quotes(char *inst)
 			if (!quote_on)
 			{
 				ft_printf("**Close quote not found\n");
-				break ;
+				return (false);
 			}
 			ft_printf("~~Found close quote %c at index value: %d\n", inst[x], x);
 		}
 	}
-	return (quote_on);
+	return (true);
+}
+
+bool	get_close_chain(char *inst, int *index)
+{
+	while (inst[++*index])
+	{
+		if (ft_isalnum(inst[*index]))
+		{
+			ft_printf("Lexer: found alpha numeric char %c for chain at index %d\n", inst[*index], *index);
+			return (true);
+		}
+	}
+	ft_printf("Lexer: no alpha numeric found for pipe\n");
+	return (false);
+}
+
+bool	validate_chains(char *inst)
+{
+	bool	chain_on;
+	int		x;
+
+	x = -1;
+	chain_on = false;
+	while (inst[++x])
+	{
+		if (inst[x] == '|' || inst[x] == '&')
+		{
+			chain_on = true;
+			if (inst[x] == '|' && inst[x + 1] == '|')
+			{
+				x++;
+				ft_printf("Lexer: Found OR\n");
+				chain_on = get_close_chain(inst, &x);
+			}
+			else if (inst[x] == '&' && inst[x + 1] == '&')
+			{
+				x++;
+				ft_printf("Lexer: Found AND\n");
+				chain_on = get_close_chain(inst, &x);
+			}
+			else if (inst[x] == '|')
+			{
+				ft_printf("Lexer: Found PIPE\n");
+				chain_on = get_close_chain(inst, &x);
+			}
+			if (!chain_on)
+				return (false);
+		}
+	}
+	return (true);
 }
 
 bool	validate(char *instruction)
 {
 	if (!instruction)
 		return (false);
-	validate_basic_quotes(instruction);
+	else if (!validate_quotes(instruction))
+	{
+		ft_printf("Lexer: Missing basic close quote\n");
+		return (false);
+	}
+	else if (!validate_chains(instruction))
+	{
+		ft_printf("Lexer: Missing chain\n");
+		return (false);
+	}
 	return (true);
 }
 
