bc6ed51a7b9881bd3f12cdd917805cb725db3485
Robin Schramm <wobula@gmail.com>
2018-01-12 13:41:58 -0800

    Normed, working.

--- src/builtins/builtin_read.c
+++ src/builtins/builtin_read.c
@@ -10,44 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "builtins.h"
 #include "ft_sh.h"
 #include <termios.h>
 
-#define READ_OPT_LR (1 << 0)
-#define READ_OPT_LU (1 << 0)
-#define READ_HAS_OPT_LR(a) (a & READ_OPT_LR)
-#define READ_HAS_OPT_LU(a) (a & READ_OPT_LU)
-
-typedef struct	s_read
-{
-	t_optparser optparser;
-	t_dstr		input;
-	int			fd;
-	char		delim;
-}				t_read;
-
-static int read_get_fd(char *u __attribute((unused)), char *fd, t_read *data)
-{
-	data->fd = ft_atoi(fd);
-	return (0);
-}
-
-static t_optsdata g_readopts =
+static t_optsdata	g_readopts =
 {
 	"read", NULL, NULL, NULL, 0, true, 0, {
 		{'r', NULL, NULL, NULL, NULL, READ_OPT_LR, 0, NULL, 0, 0},
-		{'u', NULL, NULL, NULL, NULL, READ_OPT_LU, 0, read_get_fd, true, 0},
+		{'u', NULL, NULL, NULL, NULL, READ_OPT_LU, 0, NULL, true, 0},
 		{0, NULL, NULL, NULL, NULL, 0, 0, NULL, 0, 0}
 	}
 };
 
-static int	read_assign(t_read *data)
-{
-	(void)data;
-	return (0);
-}
-
-static int	read_loop(t_read *data)
+static int			read_loop(t_read *data)
 {
 	int		ret;
 	int		esc;
@@ -58,9 +34,6 @@ static int	read_loop(t_read *data)
 	{
 		if ((ret = read(data->fd, buf, 5) <= 0))
 			return (ret);
-		if (*buf == '\b')
-			DBG("It's a backspace");
-		DBG("%d", *buf);
 		if (ret > 1)
 			continue ;
 		buf[1] = '\0';
@@ -79,20 +52,7 @@ static int	read_loop(t_read *data)
 	return (0);
 }
 
-void	read_settings(t_read *read)
-{
-	(void)read;
-	struct termios term;
-
-	tcgetattr(STDIN, &term);
-	term.c_lflag |= (ICANON | ECHO);
-	term.c_cc[VMIN] = 1;
-	term.c_cc[VTIME] = 0;
-	term.c_cc[0] = 0;
-	tcsetattr(STDIN, TCSANOW, &term);
-}
-
-int	builtin_read(char *av[])
+int					builtin_read(char **av)
 {
 	t_read	data;
 	int		ret;
@@ -104,11 +64,8 @@ int	builtin_read(char *av[])
 	data.fd = 0;
 	data.delim = '\n';
 	ft_dstr_new(&data.input, 24);
-	read_settings(&data);
 	ret = read_loop(&data);
 	DBG("%s", ((t_dstr)data.input).data);
-	if (!ret)
-		ret = read_assign(&data);
 	ft_dstr_free(&data.input);
 	return (ret);
 }
