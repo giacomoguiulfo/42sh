b74d6ec6b5b05ee7efbd0eb031bb10b89d27857e
Robin Schramm <wobula@gmail.com>
2018-01-21 12:48:41 -0800

    Small norm.

--- includes/execute.h
+++ includes/execute.h
@@ -17,14 +17,14 @@
 # include "lexer.h"
 # include "parser.h"
 
-typedef struct	s_here
+typedef struct		s_here
 {
-	char		*new_instr;
-	char		*prompt;
-	char		*safe_word;
-	char		buff[4092];
-	int			ret;
-}				t_here;
+	char			*new_instr;
+	char			*prompt;
+	char			*safe_word;
+	char			buff[4092];
+	int				ret;
+}					t_here;
 
 typedef struct		s_pipeline
 {
@@ -36,27 +36,27 @@ typedef struct		s_pipeline
 	int				pipefd[2];
 	struct s_astree	*node;
 	struct s_shell	*shell;
-	struct s_astree	*end;	
+	struct s_astree	*end;
 }					t_pipeline;
 
-void			check_pipes(t_astree *node);
-void			execute_ast_cmds(t_astree *head);
-t_astree		*piped_execution(t_astree *node, char *path);
-void			redirect_heredoc(t_tokelist *redir);
-void			restore_io(t_shell *shell);
-void			setup_io(t_shell *shell, t_tokelist **redirs);
+void				check_pipes(t_astree *node);
+void				execute_ast_cmds(t_astree *head);
+t_astree			*piped_execution(t_astree *node, char *path);
+void				redirect_heredoc(t_tokelist *redir);
+void				restore_io(t_shell *shell);
+void				setup_io(t_shell *shell, t_tokelist **redirs);
 
-bool			try_without_path(char *test);
-int				msh_run_prog(char *executable, char **args, char **newenvp);
-char			*build_bin_path(char *path, char *binary);
+bool				try_without_path(char *test);
+int					msh_run_prog(char *executable, char **args, char **newenvp);
+char				*build_bin_path(char *path, char *binary);
 
-bool			check_access(char *path);
-bool			check_builtin(char *binary);
-bool			check_reg_file(mode_t st_mode);
-t_builtin		*msh_run_builtin(char *name);
+bool				check_access(char *path);
+bool				check_builtin(char *binary);
+bool				check_reg_file(mode_t st_mode);
+t_builtin			*msh_run_builtin(char *name);
 
-t_astree		*get_end(t_astree *node);
-int				piped_fork(t_pipeline this, int in, int out);
-void			make_process(t_pipeline this, int in, int out);
+t_astree			*get_end(t_astree *node);
+int					piped_fork(t_pipeline this, int in, int out);
+void				make_process(t_pipeline this, int in, int out);
 
 #endif
